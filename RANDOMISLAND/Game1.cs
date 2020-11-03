23cbf0deb2cab9116c1df6bae5c1a7f0c9a4a81e
MCREAPER-dotcom <artempohlebaev2610@gmail.com>
2020-11-03 14:41:09 +0500

    Создание трёх новых классов для запуска стартового экрана

--- RANDOMISLAND/Game1.cs
+++ RANDOMISLAND/Game1.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
+using Microsoft.Xna.Framework.Content;
 namespace RANDOMISLAND
 {
     public class Game1 : Game
@@ -11,6 +11,7 @@ namespace RANDOMISLAND
 
         public Game1()
         {
+
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
@@ -18,24 +19,27 @@ namespace RANDOMISLAND
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            _graphics.PreferredBackBufferWidth=(int)ScreenManager.Instance.Dimensions.X;
+            _graphics.PreferredBackBufferHeight = (int)ScreenManager.Instance.Dimensions.Y;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            ScreenManager.Instance.LoadContent(Content);
         }
-
+        protected override void UnloadContent()
+        {
+            ScreenManager.Instance.UnloadContent();
+        }
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            ScreenManager.Instance.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -44,7 +48,7 @@ namespace RANDOMISLAND
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            ScreenManager.Instance.Draw(_spriteBatch);
 
             base.Draw(gameTime);
         }
